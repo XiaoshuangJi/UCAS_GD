aa79c6318976ce31e6240657e868ef7d0a776d2c
XiaoshuangJi <101794520+XiaoshuangJi@users.noreply.github.com>
2025-01-10 17:03:42 +0800

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,7 +1,7 @@
 import requests
 
-KeyID = "2054ef3cc0a64b5daf0c3afc54f7ffae"
-Secret = "6E4D50404DCC641599EAB7807D138CFA"
+KeyID = "Your keyid"
+Secret = "Your secret"
 url = "http://cloud.gstore.cn/api"
 sparql = "PREFIX : <file:///F:/d2r-server-0.7/holder8.nt#holder_copy/>" \
          "select (cycleBoolean(:{}, :{}, true, {{}}) as ?x)where{{ }}"
